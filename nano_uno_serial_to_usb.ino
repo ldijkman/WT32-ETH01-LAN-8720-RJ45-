86c1a515b3eb6540ac236d47bbbe7ab8f1016749
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2021-03-27 10:25:55 +0100

    Update nano_uno_serial_to_usb.ino

--- nano_uno_serial_to_usb.ino
+++ nano_uno_serial_to_usb.ino
@@ -1,1 +1,18 @@
+// ftdi serial monitor loses track/connection with wt32-eth01
+// a nano uno serial to usb shows all the serial data on serial monitor
+//
+int incomingByte = 0; // for incoming serial data
 
+void setup() {
+  Serial.begin(115200); // opens serial port, sets data rate to 9600 bps
+}
+
+void loop() {
+  
+  if (Serial.available() > 0) {            // send data only when you receive data:
+    
+    incomingByte = Serial.read();         // read the incoming byte from WT32-ETH01 TX0 RX0 :
+
+    Serial.write(incomingByte);          // send incomming on RX TX via usb to serial monitor
+  }
+}
